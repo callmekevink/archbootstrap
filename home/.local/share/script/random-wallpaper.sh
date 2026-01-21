47d23b6f1b698673b43c6587198f6ceb6e05fb95
callmekevink <kevin.kuffner@ausbildung-bbw.de>
2026-01-21 21:33:45 +0100

    Update random-wallpaper.sh

--- home/.local/share/script/random-wallpaper.sh
+++ home/.local/share/script/random-wallpaper.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# 1. Manually set the PATH so the script can find 'awww' and 'swww'
 PATH="/usr/local/bin:/usr/bin:/bin:/home/kevin/.local/bin:$PATH"
 
-# 2. Use the full path for your wallpaper dir to be safe
 WALLPAPER_DIR="$HOME/.local/share/wallpapers"
-
-# 3. Wait for the background daemon
-sleep 1 
+ 
 
 # 4. The logic
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" \) | shuf -n 1)
