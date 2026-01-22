1514bfa2958118bf89d9f707553b8f6cfc935168
callmekevink <kevin.kuffner@ausbildung-bbw.de>
2026-01-22 14:31:31 +0100

    Update random-wallpaper.sh

--- home/.local/share/script/random-wallpaper.sh
+++ home/.local/share/script/random-wallpaper.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-PATH="/usr/local/bin:/usr/bin:/bin:/home/kevin/.local/bin:$PATH"
+PATH="$HOME/.local/bin:/usr/local/bin:/usr/bin:/bin:$PATH"
 
-WALLPAPER_DIR="$HOME/.local/share/wallpapers"
- 
-
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" \) | shuf -n 1)
+DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+WALLPAPER_DIR="$DATA_HOME/wallpapers"
 
 
-if [ -n "$WALLPAPER" ]; then
+[ -d "$WALLPAPER_DIR" ] || exit 0
+
+WALLPAPER="$(find "$WALLPAPER_DIR" -type f \( -iname '*.jpg' -o -iname '*.png' \) 2>/dev/null | shuf -n 1)"
+
+if [ -n "$WALLPAPER" ] && command -v awww >/dev/null 2>&1; then
     awww img "$WALLPAPER" \
-      --transition-type wave \
-      --transition-fps 120 \
-      --transition-duration 3.0
+        --transition-type wave \
+        --transition-fps 120 \
+        --transition-duration 3.0
 fi
